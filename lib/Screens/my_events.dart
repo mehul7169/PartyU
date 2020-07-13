40dcf47130cecb1a4df0b246541b9be435126164
Samuel Atefah <satefah1@jhu.edu>
2020-07-13 12:32:43 -0400

    added addMapButton function to my_events.dart

--- lib/Screens/my_events.dart
+++ lib/Screens/my_events.dart
@@ -2,11 +2,32 @@ import 'package:flutter/material.dart';
 
 class MyEvents extends StatelessWidget {
   final bgColor = const Color(0xFFEAF0F1);
+  final black = const Color(0xFF2C3335);
+
+  FloatingActionButton addMapButton() {
+    return FloatingActionButton(
+      backgroundColor: bgColor,
+      shape: CircleBorder(
+          side: BorderSide(
+        color: black,
+        width: 4.0,
+      )),
+      child: Icon(
+        Icons.map,
+        color: black,
+      ),
+      onPressed: () {},
+    );
+  }
 
   Widget build(context) {
     return MaterialApp(
         home: Scaffold(
       backgroundColor: bgColor,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 325.00),
+        child: addMapButton(),
+      ),
       body: SafeArea(
           child: Container(
         padding: EdgeInsets.all(30),
--- lib/main.dart
+++ lib/main.dart
@@ -3,5 +3,5 @@ import 'package:profile_page/Screens/my_events.dart';
 import 'Screens/app.dart';
 
 void main() {
-  runApp(App());
+  runApp(MyEvents());
 }
