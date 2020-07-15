38e74acff24e8ac7f3d6e6b425f85e5292998cb5
Samuel Atefah <satefah1@jhu.edu>
2020-07-15 03:39:58 -0400

    progress on events dropdown list

--- lib/Event.dart
+++ lib/Event.dart
@@ -2,8 +2,8 @@ class Event {
   final String eventName;
   final String date;
   final String time;
-  final double friends;
-  final double followers;
+  final int friends;
+  final int followers;
 
   Event(this.eventName, this.date, this.time, this.friends, this.followers);
 }
--- lib/Screens/events_dropdown_view.dart
+++ lib/Screens/events_dropdown_view.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:profile_page/Event.dart';
 
 class EventsDropdownView extends StatelessWidget {
   final bgColor = const Color(0xFFEAF0F1);
   final black = const Color(0xFF2C3335);
 
-  final List<String> events = [
-    'HomeSlyce Karaoke Night',
-    'WAWA X Phi Mu Mixer',
-    'Phi Psi Party',
-    'WAWA Hopkins State',
-    'Unnamed Event',
-    'Unnamed Event',
-    'Unnamed Event',
-    'Unnamed Event',
+  final List<Event> events = [
+    Event('HomeSlyce Karaoke Night', 'Tue April 14', '8:00 PM', 2, 7),
+    Event('WAWA X Phi Mu Mixer', 'Tue April 17', '2:00 PM', 7, 21),
+    Event('Phi Psi Party', 'Fri April 24', '10:30 PM', 0, 23),
+    Event('WAWA Hopkins State', 'Sat April 25', '2:00 PM', 2, 7),
+    Event('Unnamed Event', 'Sun April 26', '10:30 PM', 7, 21),
+    Event('Unnamed Event', 'Mon April 27', '8:00 PM', 0, 23),
+    Event('Unnamed Event', 'Tue April 28', '2:00 PM', 2, 7),
+    Event('Unnamed Event', 'Wed April 29', '10:30 PM', 7, 21),
   ];
   @override
   Widget build(context) {
@@ -24,13 +25,54 @@ class EventsDropdownView extends StatelessWidget {
   }
 
   Widget buildEventCard(BuildContext context, int index) {
+    final event = events[index];
     return Container(
-        child: Card(
-            child: Column(
-      children: <Widget>[
-        Text(index.toString()),
-        Text(events[index]),
-      ],
-    )));
+      child: Card(
+          child: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 5,
+          horizontal: 15.0,
+        ),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8.0),
+              child: Row(children: <Widget>[
+                Text(event.date),
+              ]),
+            ),
+            IntrinsicHeight(
+              child: Row(children: <Widget>[
+                Text(event.time),
+                VerticalDivider(
+                  thickness: 1.0,
+                  color: Colors.black,
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(event.eventName),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.person_outline, size: 25),
+                        Icon(Icons.person_outline, size: 25),
+                        Icon(Icons.person_outline, size: 25),
+                        Column(
+                          children: <Widget>[
+                            Text(event.friends.toString() +
+                                ' Friends, ' +
+                                event.followers.toString() +
+                                'Followers'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ]),
+            ),
+          ],
+        ),
+      )),
+    );
   }
 }
