d4dd5248a937b36cdeff3ca4698157adfe8e9ab0
Samuel Atefah <satefah1@jhu.edu>
2020-07-15 00:26:29 -0400

    appears to be working successfully

--- lib/Screens/my_events.dart
+++ lib/Screens/my_events.dart
@@ -56,6 +56,53 @@ class MyEvents extends StatelessWidget {
     );
   }
 
+  Container addEventBox(String eventName, String date, String time) {
+    return Container(
+        padding: EdgeInsets.only(
+          bottom: 30.0,
+          top: 20.0,
+          right: 15.0,
+          left: 10.0,
+        ),
+        decoration: BoxDecoration(
+          color: Color(0xFFDAE0E2),
+          border: Border.all(width: 2, color: Colors.black),
+          borderRadius: const BorderRadius.all(const Radius.circular(10)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            AutoSizeText(
+              eventName,
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 23,
+              ),
+              maxLines: 2,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            AutoSizeText(
+              date,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+              ),
+              maxLines: 2,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(time,
+                style: TextStyle(
+                  fontSize: 23,
+                  fontWeight: FontWeight.w500,
+                ))
+          ],
+        ));
+  }
+
   Widget build(context) {
     return MaterialApp(
         home: Scaffold(
@@ -67,35 +114,78 @@ class MyEvents extends StatelessWidget {
       body: SafeArea(
           child: Container(
               padding: EdgeInsets.all(15),
-              child: Row(
+              child: Column(
                 children: <Widget>[
+                  //Expanded(
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text('My Invites',
+                            style: TextStyle(
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.w800,
+                            )),
+                      ),
+                      addInviteBox('WAWA Hopkins State', '4/25'),
+                      SizedBox(height: 5),
+                      addInviteBox('WAWA X Phi Mu Mixer', '4/17'),
+                      SizedBox(height: 5),
+                      addInviteBox('HomeSlyce Karoke Night', '4/14'),
+                      SizedBox(height: 5),
+                      addInviteBox('Unnamed Event', '4/15'),
+                      SizedBox(height: 5),
+                      Text(
+                        'Load More',
+                        style: TextStyle(fontSize: 13.0),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                  //),
                   Expanded(
                     child: Column(
                       children: <Widget>[
                         Container(
-                          alignment: Alignment.centerLeft,
-                          child: Text('My Invites',
-                              style: TextStyle(
-                                fontSize: 18.0,
-                                fontWeight: FontWeight.w800,
-                              )),
+                            alignment: Alignment.centerLeft,
+                            child: Text('My Events',
+                                style: TextStyle(
+                                  fontSize: 18.0,
+                                  fontWeight: FontWeight.w800,
+                                ))),
+                        SizedBox(height: 5),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: addEventBox('HomeSlyce Trivia Night',
+                                  'Friday, March 6', '9:00 PM'),
+                            ),
+                            SizedBox(width: 10),
+                            Expanded(
+                                child: addEventBox('WAWA Pregame',
+                                    'Friday, March 6th', '10:30 PM')),
+                          ],
                         ),
-                        addInviteBox('WAWA Hopkins State', '4/25'),
                         SizedBox(height: 5),
-                        addInviteBox('WAWA X Phi Mu Mixer', '4/17'),
-                        SizedBox(height: 5),
-                        addInviteBox('HomeSlyce Karoke Night', '4/14'),
-                        SizedBox(height: 5),
-                        addInviteBox('Unnamed Event', '4/15'),
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                                child: addEventBox('WAWA Spring Break Party',
+                                    'Saturday, March 7th', '2:00 PM')),
+                            SizedBox(width: 10),
+                            Expanded(
+                                child: addEventBox('Women\'s Lax Party',
+                                    'Saturday, March 7th', '11:00 PM')),
+                          ],
+                        ),
                         SizedBox(height: 5),
                         Text(
                           'Load More',
                           style: TextStyle(fontSize: 13.0),
-                        )
+                        ),
                       ],
                     ),
-                  ),
-                  Row()
+                  )
                 ],
               ))),
     ));
