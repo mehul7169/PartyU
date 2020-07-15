73521eb14acf9bd57b7b57254670ec4c6b0edbad
Samuel Atefah <satefah1@jhu.edu>
2020-07-15 02:54:04 -0400

    created Event .dart file/class

---
+++ lib/Event.dart
@@ -0,0 +1,9 @@
+class Event {
+  final String eventName;
+  final String date;
+  final String time;
+  final double friends;
+  final double followers;
+
+  Event(this.eventName, this.date, this.time, this.friends, this.followers);
+}
