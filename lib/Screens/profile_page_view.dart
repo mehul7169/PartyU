da8aeb2be53c4020b89284c18f76035eed651ed1
Samuel Atefah <satefah1@jhu.edu>
2020-07-26 04:13:47 -0400

    added addButton, addLogout functions

--- lib/Screens/profile_page_view.dart
+++ lib/Screens/profile_page_view.dart
@@ -10,8 +10,8 @@ class ProfilePageView extends StatelessWidget {
   final bgColor = const Color(0xFFffffff);
   final black = const Color(0xFF2C3335);
 
-  // Add 'Edit Profile' button.
   //TODO: Need to allow user to edit profile once button is pressed.
+  // Add 'Edit Profile' button.
   Container _addEditProfile(double screenWidth, double screenHeight) {
     return Container(
       width: screenWidth * 0.32,
@@ -50,10 +50,11 @@ class ProfilePageView extends StatelessWidget {
     );
   }
 
-  // Displays the user's picture, first name, last name, and the edit profile button.
+  // Displays the user's picture, first name, last name, and the edit
+  // profile button.
   Row _addProfile(double screenWidth, double screenHeight) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: <Widget>[
         Icon(
           Icons.portrait,
@@ -87,21 +88,29 @@ class ProfilePageView extends StatelessWidget {
     );
   }
 
-/* TODO: This function will need to be edited in order to add actual pictures of friends
-   and allow for the user to click on the icons to see the page of the friend
-*/
-// Given a number, generates the number of friends.
-  Row _addFriends(int count) {
+  // TODO: This function will need to be edited in order to add actual pictures
+  // of friends and allow for the user to click on the icons to see the page
+  // of the friend.
+
+  // Given a number, generates the number of friends.
+  Row _addFriends(int count, screenWidth) {
     List<CircleAvatar> list = new List<CircleAvatar>();
 
     for (int i = 0; i < count; i++) {
-      list.add(new CircleAvatar(
-        backgroundColor: Color(0xFF5e667e),
-        child: Icon(
-          Icons.person,
-          color: Colors.white,
+      list.add(
+        new CircleAvatar(
+          radius: screenWidth * 0.07,
+          backgroundColor: Color(0xFF5e667e),
+          child: IconButton(
+            icon: Icon(
+              Icons.person,
+            ),
+            color: Colors.white,
+            iconSize: screenWidth * 0.07,
+            onPressed: () {},
+          ),
         ),
-      ));
+      );
     }
 
     return Row(
@@ -110,10 +119,103 @@ class ProfilePageView extends StatelessWidget {
     );
   }
 
+  // Given a button label, an icon, the width of the screen, a button is
+  // created. If topOrBot equals 0, a top border will be displayed.
+  // Otherwise, a bottom border will be displayed.
+  Container _addButton(String name, IconData icon, double screenWidth,
+      double screenHeight, int topOrBot) {
+    double noWidth = 0;
+    double sideWidth = screenWidth * 0.0035;
+    double topSide;
+    double botSide;
+    Color topColor;
+    Color botColor;
+
+    if (topOrBot == 0) {
+      topSide = sideWidth;
+      botSide = noWidth;
+      topColor = Colors.black26;
+      botColor = Colors.white;
+    } else {
+      topSide = noWidth;
+      botSide = sideWidth;
+      topColor = Colors.white;
+      botColor = Colors.black26;
+    }
+    return Container(
+      width: screenWidth * 0.88,
+      height: screenHeight * 0.06,
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(width: topSide, color: topColor),
+          bottom: BorderSide(width: botSide, color: botColor),
+        ),
+      ),
+      child: FlatButton(
+        padding: EdgeInsets.all(0),
+        child: Row(
+          children: <Widget>[
+            SizedBox(
+              width: screenWidth * 0.02,
+            ),
+            Icon(icon, size: 35),
+            SizedBox(
+              width: screenWidth * 0.02,
+            ),
+            Text(
+              name,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: screenWidth * 0.04,
+              ),
+            ),
+            Spacer(),
+            Icon(Icons.keyboard_arrow_right, size: 35),
+            SizedBox(
+              width: screenWidth * 0.02,
+            ),
+          ],
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  // Generates the the "Logout" button.
+  Container _addLogoutButton(double screenWidth, double screenHeight) {
+    return Container(
+      width: screenWidth * 0.88,
+      height: screenHeight * 0.06,
+      child: FlatButton(
+        padding: EdgeInsets.all(0),
+        child: Row(
+          children: <Widget>[
+            SizedBox(
+              width: screenWidth * 0.125,
+            ),
+            Text(
+              'Logout',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: screenWidth * 0.04,
+              ),
+            ),
+            Spacer(),
+            Icon(Icons.keyboard_arrow_right, size: 35),
+            SizedBox(
+              width: screenWidth * 0.02,
+            ),
+          ],
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
   // Displays the "My Friends & Groups" box.
   Container _addFriendsAndGroups(screenWidth, screenHeight) {
     return Container(
-      height: screenHeight * 0.22,
+      height: screenHeight * 0.70,
       width: screenWidth * 0.88,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -128,40 +230,68 @@ class ProfilePageView extends StatelessWidget {
             maxLines: 1,
           ),
           Container(
-            padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
-            decoration: BoxDecoration(
-              border: Border.all(width: 1.2, color: Colors.black26),
-              borderRadius: const BorderRadius.all(const Radius.circular(6)),
-            ),
-            child: Column(
+            height: screenHeight * 0.65,
+            width: screenWidth * 0.88,
+            child: ListView(
+              padding: EdgeInsets.only(
+                top: 5,
+              ),
               children: <Widget>[
-                _addFriends(5),
-                Divider(thickness: 1.2, color: Colors.black26),
-                //TODO: Make this a separate function.
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.near_me, size: 35),
-                    Text(
-                      '   Friends & Followers',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Spacer(),
-                    Icon(Icons.keyboard_arrow_right, size: 35),
-                  ],
+                Container(
+                  padding: const EdgeInsets.only(
+                    top: 10,
+                    bottom: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        width: screenWidth * 0.0035, color: Colors.black26),
+                    borderRadius:
+                        const BorderRadius.all(const Radius.circular(6)),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        height: screenHeight * 0.07,
+                        padding: EdgeInsets.only(
+                          bottom: screenHeight * 0.02,
+                          //top: screenHeight * 0.003,
+                        ),
+                        child: _addFriends(5, screenWidth),
+                      ),
+                      _addButton('Friends & Followers', Icons.near_me,
+                          screenWidth, screenHeight, 0),
+                      _addButton('Groups', Icons.notifications_none,
+                          screenWidth, screenHeight, 0),
+                    ],
+                  ),
                 ),
-                Divider(thickness: 1.2, color: Colors.black26),
-                //TODO: Make this a separate function.
-                Row(
-                  children: <Widget>[
-                    Icon(Icons.notifications_none, size: 35),
-                    Text(
-                      '   Groups',
-                      style: TextStyle(fontWeight: FontWeight.bold),
+                SizedBox(
+                  height: screenHeight * 0.025,
+                ),
+                Container(
+                  padding: const EdgeInsets.only(
+                    top: 5,
+                    bottom: 5,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: screenWidth * 0.0035,
+                      color: Colors.black26,
                     ),
-                    Spacer(),
-                    Icon(Icons.keyboard_arrow_right, size: 35),
-                  ],
+                    borderRadius:
+                        const BorderRadius.all(const Radius.circular(6)),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      _addButton('Location Preferences', Icons.near_me,
+                          screenWidth, screenHeight, 1),
+                      _addButton('Notifications', Icons.notifications_none,
+                          screenWidth, screenHeight, 1),
+                      _addButton('Settings', Icons.settings, screenWidth,
+                          screenHeight, 1),
+                      _addLogoutButton(screenWidth, screenHeight),
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -179,6 +309,8 @@ class ProfilePageView extends StatelessWidget {
       home: Scaffold(
         backgroundColor: bgColor,
         body: Container(
+          width: _width,
+          height: _height,
           padding: EdgeInsets.symmetric(
             horizontal: _width * 0.06,
             vertical: _height * 0.04,
@@ -188,69 +320,6 @@ class ProfilePageView extends StatelessWidget {
             children: <Widget>[
               _addProfile(_width, _height),
               _addFriendsAndGroups(_width, _height),
-
-              //TODO: Make this a separate function.
-              Container(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
-                decoration: BoxDecoration(
-                  border: Border.all(width: 1.2, color: Colors.black26),
-                  borderRadius:
-                      const BorderRadius.all(const Radius.circular(6)),
-                ),
-                child: Column(
-                  children: <Widget>[
-                    //TODO: Make this a separate function.
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.near_me, size: 35),
-                        Text(
-                          '   Location Preferences',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Spacer(),
-                        Icon(Icons.keyboard_arrow_right, size: 35),
-                      ],
-                    ),
-                    Divider(thickness: 1.2, color: Colors.black26),
-                    //TODO: Make this a separate function.
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.notifications_none, size: 35),
-                        Text(
-                          '   Notifications',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Spacer(),
-                        Icon(Icons.keyboard_arrow_right, size: 35),
-                      ],
-                    ),
-                    Divider(thickness: 1.2, color: Colors.black26),
-                    //TODO: Make this a separate function.
-                    Row(
-                      children: <Widget>[
-                        Icon(Icons.settings, size: 35),
-                        Text(
-                          '   Settings',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Spacer(),
-                        Icon(Icons.keyboard_arrow_right, size: 35),
-                      ],
-                    ),
-                    Divider(thickness: 1.2, color: Colors.black26),
-                    //TODO: Make this a separate function.
-                    Row(
-                      children: <Widget>[
-                        Text(
-                          '             Logout',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
             ],
           ),
         ),
